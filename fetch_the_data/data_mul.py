54dbe3593a68da31ff91765b558840164d9f5fd9
Kavi <132807842+kavikathiresan@users.noreply.github.com>
2023-06-06 09:31:38 +0530

    Add files via upload

---
+++ fetch_the_data/data_mul.py
@@ -0,0 +1,15 @@
+class connection():
+    def multiplication(self,a,b):
+        return a * b
+    def add(self,a,b):
+        return a+b
+obj=connection()
+obj.multiplication(4,5)
+obj.add(3,7)
+
+
+
+
+
+
+
